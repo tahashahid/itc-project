3fe3d4f61326f612b159d406f8b4c1ab26857dad
tahashahid <m.tahashahid@yahoo.com>
2017-12-19 21:07:16 +0500

    implemented logout

--- scripts/controllers/header.js
+++ scripts/controllers/header.js
@@ -1,4 +1,9 @@
 
-window.APP.controller("headerCtrl", function($scope, $localStorage){
+window.APP.controller("headerCtrl", function($scope, $localStorage, $state){
     $scope.userName = `${$localStorage.user.username} ${$localStorage.user.lastName || " "}`;
+
+    $scope.logout = function(){
+        $localStorage.user = null;
+        $state.go("login");
+    }
 })
